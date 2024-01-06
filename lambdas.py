7e3c93a09979717277317717d03ecdaf6e1b69f4
Marcus Vinicius Lima da Costa <marcusv2001@gmail.com>
2024-01-05 23:21:53 -0300

    just some python learning

--- lambdas.py
+++
@@ -1,4 +0,0 @@
-
-full_name = lambda name, surname = 'Vinicius': name.strip().title() + ' ' + surname.strip().title()
-
-print(full_name(' marcus '))
---
+++ learning.py
@@ -0,0 +1,55 @@
+"""
+# Lambda, map and zip
+
+import math 
+
+radiusList = [2, 5, 6, 12]
+
+areas = map(lambda radius: math.pi * (radius ** 2), radiusList)
+areas_dict = dict(zip(radiusList, areas))
+
+print(areas_dict)
+
+# celsius to farenheit conversion = 9/5 * c + 32
+
+cities = [('Berlin', 1), ('London', 5), ('New York', 8), ('Rio de Janeiro', 23)]
+
+celsius_to_farenheit = lambda data: [data[0], 9/5 * data[1] + 32]
+
+cities_converted = map(celsius_to_farenheit, cities)
+
+print(list(cities_converted))
+
+# Filter
+
+countries = ['Germany', '', 'Belgium', 'Brazil', '', 'Argentina']
+
+countries_filtered = filter(lambda x: x != '', countries)
+
+print(list(countries_filtered))
+
+users = [
+    {'user': 'Bob', 'messages': []},
+    {'user': 'Richard', 'messages': ['I love my pets', 'I am going out today']},
+    {'user': 'Jeff', 'messages': ['Going to Work!']}
+]
+
+inactive_users = filter(lambda user: not user['messages'], users)
+
+active_users = filter(lambda user: user['messages'], users)
+
+print(f'List of inactive users > {[user["user"] for user in inactive_users]}')
+print(f'List of active users > {[user["user"] for user in active_users]}')
+
+# Reduce
+
+from functools import reduce
+
+numbers = [1, 2, 3, 4, 5, 6]
+
+sum = reduce(lambda a, b: a + b, numbers)
+
+print(sum)
+
+"""
+
