b92f35f8fb56093408fd9d2a97d15acd551681e9
Marcus Vinicius Lima da Costa <marcusv2001@gmail.com>
2024-01-06 21:53:21 -0300

    home moved to templates

--- recipes/views.py
+++ recipes/views.py
@@ -1,14 +1,7 @@
 from django.shortcuts import render
-from django.http import HttpResponse
 
 # Create your views here.
 
 def home(request):
-    return HttpResponse('''
-                        <!DOCTYPE>
-                        <html>
-                            <head> <title> Hello World </title> <head>
-                            <body> <h1>  Hello  World! </body>
-                        </html>
-                        ''')
+    return render(request, 'recipes/home.html')
 
